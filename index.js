4290df669f86c2f4f50f806b21ef27bbeebe39d4
Daria Skoryk <dariaskoryk@gmail.com>
2023-09-24 23:40:40 +0200

    final result

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const question = document.querySelector('.question');
+question.textContent =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
 
+const answer = prompt('give me an answer');
+
+const answerElement = document.querySelector('.answer__text');
+answerElement.textContent = answer;
+
+if (answer === 'Bell') {
+  document.querySelector('.answer').classList.add('answer--correct');
+} else {
+  document.querySelector('.answer').classList.add('answer--wrong');
+}
